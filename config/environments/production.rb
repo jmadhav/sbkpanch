910dac1fdfda45386ee4eaf6e090c94e8c1d0836
Jai <jai@jai-Aspire-5742.(none)>
2013-06-07 10:55:48 +0530

    Added Host for Action Mailer 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ PhaseOne::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.default_url_options = { :host => 'sbkpanch-stage.herokuapp.com' }
+
   # ExceptionNotifier
   config.middleware.use ExceptionNotifier,
     email_prefix: "SBKWEB-Errors: ",
