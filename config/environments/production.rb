55ade9573d298c2cda9e209533c3b3ae235314e2
jmadhav <jai.madhav86@gmail.com>
2013-07-21 17:10:07 +0530

    Set default_url_options to sbkpanch.com 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ PhaseOne::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'sbkpanch-stage.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sbkpanch.com' }
 
   # ExceptionNotifier
   config.middleware.use ExceptionNotifier,
